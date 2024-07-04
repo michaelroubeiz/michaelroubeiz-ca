3a1da8dee8aaa2ef85d80a7753a45a19a29c08bc
Michael Roubeiz <michaelroubeiz@gmail.com>
2024-07-04 19:59:38 -0400

    Add insights for vercel

--- app.vue
+++ app.vue
@@ -1,9 +1,18 @@
 <template>
   <NuxtLayout>
+    <SpeedInsights />
     <NavBar />
     <NuxtPage class="max-w-3xl m-auto mt-16 pb-16" />
   </NuxtLayout>
 </template>
+
+<script lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+
+export default {
+  components: { SpeedInsights },
+};
+</script>
 
 <style>
 body {
