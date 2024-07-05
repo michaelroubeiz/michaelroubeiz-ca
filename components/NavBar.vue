c9a99c3a9ada353c3b04340c656bda6c0718d608
Michael Roubeiz <michaelroubeiz@gmail.com>
2024-07-05 18:34:20 -0400

    Small nav css changes

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,19 @@
 <template>
   <div
-    class="mt-5 md:px-8 px-4 border-b border-gray-200 dark:border-gray-800 justify-between flex flex-row items-center"
+    class="mt-5 md:px-8 px-4 border-b border-gray-800 justify-between flex flex-row items-center text-white"
   >
     <ULink to="/" class="font-playwrite">MR</ULink>
-    <UHorizontalNavigation :links="links" class="w-auto font-roboto-mono md:scale-100 scale-90 md:translate-x-0 translate-x-4" />
+    <UHorizontalNavigation
+      :links="links"
+      class="w-auto md:scale-100 scale-90 md:translate-x-0 translate-x-4 hover:bg-black-300"
+    >
+      <template #default="{ link }">
+        <span
+          class="font-roboto-mono text-white after:border-gray-300 hover:text-white dark:text-white after:bg-red-500 dark:after:bg-red-400"
+          v-text="link.label"
+        ></span>
+      </template>
+    </UHorizontalNavigation>
   </div>
 </template>
 
