081fbd42bfad86e824a63be5ed79e2132d66041f
Michael Roubeiz <michaelroubeiz@gmail.com>
2024-07-18 13:26:51 -0400

    Adjust text size for projects

--- components/Projects/ProjectItem.vue
+++ components/Projects/ProjectItem.vue
@@ -3,7 +3,7 @@
     class="border-blue-900/50 hover:border-blue-900 border-2 rounded-lg p-6 hover:-translate-y-1 transition ease-out delay-150 h-full"
   >
     <div class="grid grid-flow-col mb-2">
-      <div class="max-w-min whitespace-nowrap font-roboto-mono">
+      <div class="max-w-min whitespace-nowrap font-roboto-mono text-xl">
         <template v-if="projectItem.projectLinkActive">
           <ULink
             :to="projectItem.projectLink"
@@ -30,7 +30,9 @@
         </UTooltip>
       </ULink>
     </div>
-    <div class="text-xs text-gray-300 mb-6">{{ projectItem.description }}</div>
+    <div class="text-base text-gray-300 mb-6">
+      {{ projectItem.description }}
+    </div>
     <div class="flex flex-wrap gap-2 mt-2">
       <div
         v-for="item in projectItem.technologies"
