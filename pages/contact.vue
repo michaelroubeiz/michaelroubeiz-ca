456d06f951565fc303d92618d201f4c4959e2a84
Michael Roubeiz <michaelroubeiz@gmail.com>
2024-07-26 12:05:44 -0400

    Add aria labels to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,14 +10,26 @@
       <div
         class="grid grid-flow-row md:grid-flow-col items-center m-8 gap-6 md:gap-24"
       >
-        <UFormGroup name="name" label="Name" size="lg" required>
+        <UFormGroup
+          name="name"
+          label="Name"
+          size="lg"
+          aria-label="Name"
+          required
+        >
           <UInput
             v-model="state.senderName"
             variant="outline"
             :ui="{ color: 'indigo' }"
           />
         </UFormGroup>
-        <UFormGroup name="email" label="Email" size="lg" required>
+        <UFormGroup
+          name="email"
+          label="Email"
+          size="lg"
+          aria-label="Email"
+          required
+        >
           <UInput
             v-model="state.senderEmail"
             placeholder="email@gmail.com"
@@ -27,7 +39,13 @@
         </UFormGroup>
       </div>
       <div class="items-center m-8">
-        <UFormGroup name="message" label="Message" size="lg" required>
+        <UFormGroup
+          name="message"
+          label="Message"
+          size="lg"
+          aria-label="Message"
+          required
+        >
           <UTextarea
             v-model="state.senderMessage"
             variant="outline"
@@ -43,6 +61,7 @@
         <button
           class="px-8 p-2 bg-blue-800/80 rounded-md text-white"
           type="submit"
+          aria-label="Send"
           :disabled="!validateForm"
           :class="{
             'opacity-50': !validateForm,
